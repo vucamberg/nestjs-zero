ed00419845d3979e4921c916df0a0ccad4f618ca
Vucamberg Santos <vucamberg@gmail.com>
2024-01-11 16:35:02 -0300

    feat: init commit

---
+++ src/app.controller.ts
@@ -0,0 +1,19 @@
+import { Body, Controller, Post } from '@nestjs/common';
+import { CreateTeamMemberBody } from './dtos/create-team-member-body';
+import { RocketMembersRepository } from './repositories/rocket-members-repository';
+
+@Controller()
+export class AppController {
+
+  constructor(private rocketMembersRepository: RocketMembersRepository){
+  
+  }
+
+  @Post('hello')
+  async getHello(@Body() body: CreateTeamMemberBody) {
+    const { name, function: memberFunction } = body;
+   
+    await this.rocketMembersRepository.create(name, memberFunction);
+   
+  }
+}
---
+++ src/repositories/prisma/prima-rocket-members-repository.ts
@@ -0,0 +1,19 @@
+import { PrismaService } from "src/database/prisma.service";
+import { RocketMembersRepository } from "../rocket-members-repository";
+import { randomUUID } from "crypto";
+import { Injectable } from "@nestjs/common";
+
+@Injectable()
+export class PrismaRocketMembersRepository implements RocketMembersRepository {
+    constructor(private prisma: PrismaService) {}
+
+    async create(name: string, memberFunction: string): Promise<void> {
+        await this.prisma.rocketTeamMember.create({
+            data: {
+                id: randomUUID(),
+                name,
+                function: memberFunction,
+            }
+        });
+    }
+}
